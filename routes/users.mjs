69678fa6f6b916d951f263f27bc82baa943804d2
hashimrzaa <hashimterminator20@gmail.com>
2024-03-14 06:46:23 +0500

    update

--- routes/users.mjs
+++ routes/users.mjs
@@ -1,60 +1,60 @@
-import express from 'express'
-import verifyToken from '../middlewares/verifytoken.mjs'
-import Users from '../models/Users.mjs'
-const router = express.Router()
+import express from "express";
+import verifyToken from "../middlewares/verifytoken.mjs";
+import Users from "../models/Users.mjs";
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-   try {
-    const users = await Users.find({})
-    res.status(200).send({ data: users })
-} catch (error) {
-       res.status(404).send({ message: 'users not found' })
-    
-   }
-})
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.status(200).send({ data: users });
+  } catch (error) {
+    res.status(404).send({ message: "users not found" });
+  }
+});
 
-router.post('/register', async (req, res) => {
-    
-    try {
-        await Users.create(req.body)
+router.post("/register", async (req, res) => {
+  try {
+    const users = await Users.create(req.body);
 
-        res.send({ message: 'User registered successfully!'})
-    } catch (e) {
-        res.status(400).send({ message: e.message })
+    res.send({ message: "User registered successfully!", users: users });
+  } catch (e) {
+    res.status(400).send({ message: e.message });
+  }  
+});
+
+router.put("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Users.findOne({ email });
+
+    if (!user) {
+      res.status(404).send({ message: "Email not found!" });
+      return;
     }
-})
 
-router.put('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body
+    const isCorrectPassword = user.comparePassword(password);
 
-        const user = await Users.findOne({ email })
-    
-        if (!user) {
-            res.status(404).send({ message: 'Email not found!' })
-            return
-        }
-    
-        const isCorrectPassword = user.comparePassword(password)
-    
-        if (!isCorrectPassword) {
-            res.status(404).send({ message: 'Password is incorrect!' })
-            return
-        }
-    
-        const token = user.generateToken()
-        user.tokens.push(token)
-        await user.save()
-    
-        res.send({ message: 'User logged in successfully!', token })
-    } catch (e) {
-        res.status(400).send({ message: e.message })
+    if (!isCorrectPassword) {
+      res.status(404).send({ message: "Password is incorrect!" });
+      return;
     }
-})
 
-router.put('/logout', verifyToken, async (req, res) => {
-    await Users.findByIdAndUpdate(req.userId, { $pull: { tokens: req.tokenToRemove } })
-    res.send({ message: 'Logged out successfully!' })
-})
+    const token = user.generateToken();
+    user.tokens.push(token);
+    await user.save();
 
-export default router
+    res.send({ message: "User logged in successfully!", token });
+  } catch (e) {
+    res.status(400).send({ message: e.message });
+  }
+});
+
+router.put("/logout", verifyToken, async (req, res) => {
+  await Users.findByIdAndUpdate(req.userId, {
+    $pull: { tokens: req.tokenToRemove },
+  });
+  res.send({ message: "Logged out successfully!" });
+});
+
+export default router;
