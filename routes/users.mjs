142525ebcad060fa694da9e58666dde54056b4fb
hashimrzaa <hashimterminator20@gmail.com>
2024-03-16 03:19:55 +0500

    update

--- routes/users.mjs
+++ routes/users.mjs
@@ -47,7 +47,7 @@ router.delete("/delete/:id", async (req, res) => {
     if (!deletedUser) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json({ message: "User deleted successfully", deletedUser });
+    res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
